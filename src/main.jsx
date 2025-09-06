b9ed143891a0c45eeb9dd63c3a88873b93ea703e
Iwebs <44825570+yairelisea@users.noreply.github.com>
2025-09-06 14:06:03 -0600

    blank view

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from '@/App';
-import '@/index.css';
-import { AuthProvider } from "@/context/AuthContext"; 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// src/main.jsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "@/App";
+import "@/index.css";
+import { AuthProvider } from "@/context/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
