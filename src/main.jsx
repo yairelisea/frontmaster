d0d7224c01d1345d077b7ab6001f4221892bd648
Iwebs <44825570+yairelisea@users.noreply.github.com>
2025-09-10 20:11:50 -0600

    okmain

--- src/main.jsx
+++ src/main.jsx
@@ -1,43 +1,11 @@
 // src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
-import AuthLayout from "./layouts/AuthLayout.jsx";
-import LoginPage from "./pages/auth/LoginPage.jsx";
-import RequireAuth from "./pages/auth/RequireAuth.jsx";
-
-import AdminLayout from "./admin/AdminLayout.jsx";
-import AdminCampaignsPage from "./admin/AdminCampaignsPage.jsx";
-import AdminCampaignDetailPage from "./pages/admin/AdminCampaignDetailPage.jsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/auth",
-    element: <AuthLayout />,
-    children: [{ path: "login", element: <LoginPage /> }],
-  },
-  {
-    element: <RequireAuth />, // 👈 protege todo lo siguiente
-    children: [
-      {
-        path: "/",
-        element: <App />,
-      },
-      {
-        path: "/admin",
-        element: <AdminLayout />,
-        children: [
-          { path: "campaigns", element: <AdminCampaignsPage /> },
-          { path: "campaigns/:id", element: <AdminCampaignDetailPage /> },
-        ],
-      },
-    ],
-  },
-]);
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
